e5f6fab82b7b877488394e29f0f4b1a3b7501e0d
Rolf Sormo <rolf.sormo@aphex.fi>
2013-10-13 07:08:47 +0300

    Fixed MongoMatcher from OR to AND.

--- test/nodeunit/base_test.js
+++ test/nodeunit/base_test.js
@@ -23,25 +23,34 @@ exports.group1 = {
   testBasicFind: function(test) {
     var self = this;
 
-    test.expect(8);
-    self.db.insert({a:'a'}, function(err, result) {
+    var a = {a:'a', c:'c'};
+    var b = {b:'b'};
+
+    test.expect(11);
+    self.db.insert(a, function(err, result) {
       test.ok(!err, 'Error');
 
-      self.db.insert({b:'b'}, function(err, result) {
+      self.db.insert(b, function(err, result) {
         test.ok(!err, 'Error');
 
         self.db.find({a:'a'}, function(err, list) {
           test.ok(!err, 'Error');
 
           test.equal(list.length, 1, 'Result list length');
-          test.deepEqual(list, [{a:'a', _id:list[0]._id}], 'List equals');
+          test.deepEqual(list, [a], 'List equals');
 
           self.db.find({a:{$ne:'a'}}, function(err, list) {
             test.ok(!err, 'Error');
 
             test.equal(list.length, 1, 'Result list length');
-            test.deepEqual(list, [{b:'b', _id:list[0]._id}], 'List equals');
-            test.done();
+            test.deepEqual(list, [b], 'List equals');
+            self.db.find({a:'a', c:'c'}, function(err, list) {
+              test.ok(!err, 'Error');
+
+              test.equal(list.length, 1, 'Result list length');
+              test.deepEqual(list, [a], 'List equals');
+              test.done();
+            });
           });
         });
       });
