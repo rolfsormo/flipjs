cd9c9d9049561b447277a6b9b72d69c840f1273b
Rolf Sormo <rolf.sormo@aphex.fi>
2013-10-15 20:53:40 +0300

    Fixed browser globals test.

--- test/qunit/globals_test.js
+++ test/qunit/globals_test.js
@@ -1,33 +1,39 @@
+window.localStorage.clear();
+
 var db;
 module('globals', {
-	setup: function() {
-		db = Flip.connect('Test', {});
-    db.collection('coll');
-	},
-	tearDown: function() {
-		db.dropDatabase();
-	}
+  setup: function() {
+    stop();
+    Flip.connect('Test', {}, function(err, _db) {
+      db = _db;
+      db.collection('coll');
+      start();
+    });
+  },
+  tearDown: function() {
+    db.dropDatabase();
+  }
 });
 
 
 test('Basics', function() {
-	expect(8);
+  expect(8);
 
-	var a = {a:'a'};
-	var b = {b:'b'};
-	db.coll.insert(a, function(err, result) {
-		ok(!err, 'Error check');
+  var a = {a:'a'};
+  var b = {b:'b'};
+  db.coll.insert(a, function(err, result) {
+    ok(!err, 'Error check');
 
-		a._id = result._id;
-		deepEqual(a, result, 'Read what we wrote');
-		db.coll.insert(b, function(err, result) {
-			ok(!err, 'Error check');
+    a._id = result._id;
+    deepEqual(a, result, 'Read what we wrote');
+    db.coll.insert(b, function(err, result) {
+      ok(!err, 'Error check');
 
       b._id = result._id;
-			db.coll.find({a:'a'}, function(err, res) {
-				ok(!err, 'Error check');
+      db.coll.find({a:'a'}, function(err, res) {
+        ok(!err, 'Error check');
 
-				deepEqual([a], res, 'Find one object');
+        deepEqual([a], res, 'Find one object');
 
         db.coll.remove({a:'a'}, function(err, res) {
           ok(!err, 'Error check');
@@ -35,10 +41,10 @@ test('Basics', function() {
           db.coll.find({}, function(err, res) {
             ok(!err, 'Error check');
 
-            deepEqual(res, [b], 'Find one object');              
+            deepEqual([b], res, 'Find one object');              
           });
         });
-			});
-		});
-	});
+      });
+    });
+  });
 });
